73d963a4a00f09a59abe5a377b3b3737b7d11c23
Andrew Weiss <wvvwwvw@gmail.com>
2018-11-30 21:14:30 -0800

    Day 1

--- 1/advent/src/main.rs
+++ 1/advent/src/main.rs
@@ -1,8 +1,37 @@
 extern crate utils;
+use std::collections::HashSet;
 
 fn main() {
-    for (i, line) in utils::get_lines("advent.txt").enumerate() {
+    let mut sum = 0;
+    let mut seen = HashSet::new();
+    let mut stream = Vec::new();
+    seen.insert(0);
+    let mut done = false;
+    for line in utils::get_lines("advent.txt") {
         let line = line.unwrap();
-        println!("{}: {}", i, line);
+        let val = line.parse::<i32>().unwrap();
+        sum += val;
+        if !seen.insert(sum) {
+            println!("Part 2: {}", sum);
+            done = true;
+            break;
+        }
+        stream.push(val);
+    }
+    if !done {
+        println!("Part 1: {}", sum);
+    } else {
+        println!("Part 1 is missing because no loop in part 2");
+    }
+
+    while !done {
+        for val in &stream {
+            sum += *val;
+            if !seen.insert(sum) {
+                println!("Part 2: {}", sum);
+                done = true;
+                break;
+            }
+        }
     }
 }
