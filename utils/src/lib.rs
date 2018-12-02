8e4e779e34b096754d570b398b000efa258a320d
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-01 21:29:39 -0800

    add some more utils functions

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -3,8 +3,18 @@ use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+pub use std::collections::{HashMap, HashSet};
+
 pub fn get_lines(filename: &str) -> impl Iterator<Item = io::Result<String>> {
     let f = File::open(filename).expect("file not found");
     let f = BufReader::new(f);
     f.lines()
+}
+
+pub fn read_file(filename: &str) -> impl Iterator<Item = String> {
+    get_lines(filename).map(|s| s.unwrap())
+}
+
+pub fn split_ws(line: &str) -> Vec<&str> {
+    line.split_whitespace().collect()
 }
