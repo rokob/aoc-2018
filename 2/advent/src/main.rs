8b99583d94bcac98229b5a40eb64d5d2428c8366
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-01 21:20:32 -0800

    day 2 part 2

--- 2/advent/src/main.rs
+++ 2/advent/src/main.rs
@@ -1,33 +1,35 @@
 extern crate utils;
-use std::collections::{HashSet, HashMap};
 
 fn main() {
-    let mut count2 = 0;
-    let mut count3 = 0;
+    let mut lines: Vec<String> = Vec::new();
     for line in utils::get_lines("advent.txt") {
         let line = line.unwrap();
-        let mut letter_count = HashMap::new();
-        for c in line.chars() {
-            let e = letter_count.entry(c).or_insert(0);
-            *e += 1;
-        }
-        let mut contains_two = false;
-        let mut contains_three = false;
-        for (k, v) in letter_count.iter() {
-            if *v == 2 {
-                contains_two = true;
+        lines.push(line);
+    }
+
+    for (i, line) in lines.iter().enumerate() {
+        for (j, o) in lines.iter().enumerate() {
+            if i == j { continue; }
+            let mut diff: u32 = 0;
+            for (a, b) in line.chars().zip(o.chars()) {
+                if a != b {
+                    diff += 1;
+                }
+                if diff > 1 {
+                    break;
+                }
             }
-            if *v == 3 {
-                contains_three = true;
+            if diff == 1 {
+                for (a, b) in line.chars().zip(o.chars()) {
+                    if a == b {
+                        print!("{}", a);
+                    }
+                }
+                println!("");
+                return;
             }
-        }
-        if contains_two {
-            count2 += 1;
-        }
-        if contains_three {
-            count3 += 1;
         }
     }
-    println!("{}", count2 * count3);
+
 }
 
