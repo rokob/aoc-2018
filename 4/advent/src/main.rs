3907f8797a3bb32a6d623590339c0e1e1127b1d5
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-02 21:14:01 -0800

    setup for day 4

---
+++ 4/advent/src/main.rs
@@ -0,0 +1,12 @@
+extern crate utils;
+#[allow(unused_imports)]
+use utils::{HashSet, HashMap, read_file, split_ws};
+
+fn main() {
+    /*
+    for line in read_file("advent.txt") {
+        let data = split_ws(&line);
+    }
+    */
+    println!("Hello, world!");
+}
