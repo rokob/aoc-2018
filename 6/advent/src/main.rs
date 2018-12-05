7d564648bc73c6bb7e9be9e0b91cb4cbb80a5f24
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-04 22:12:52 -0800

    prep for day 6

---
+++ 6/advent/src/main.rs
@@ -0,0 +1,11 @@
+extern crate utils;
+#[allow(unused_imports)]
+use utils::{HashSet, HashMap, read_file, split_ws};
+
+fn main() {
+    /*
+    for line in read_file("advent.txt") {
+        let claim = split_ws(&line);
+    }
+    */
+}
