8517a5976eab79e36a641f856de20476a9d6d0cc
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-04 21:34:10 -0800

    day 5 part 1

--- 5/advent/src/main.rs
+++ 5/advent/src/main.rs
@@ -3,9 +3,49 @@ extern crate utils;
 use utils::{HashSet, HashMap, read_file, split_ws};
 
 fn main() {
-    /*
+    let mut polymer = Vec::new();
+    let mut last_char = ' ';
     for line in read_file("advent.txt") {
-        let data = split_ws(&line);
+        for c in line.chars() {
+            if is_match(last_char, c) {
+                polymer.pop();
+                last_char = ' ';
+                continue;
+            }
+            polymer.push(c);
+            last_char = c;
+        }
     }
-    */
+
+    let mut no = false;
+    while !no {
+        no = true;
+        last_char = ' ';
+        let mut new_polymer = Vec::new();
+        for c in polymer.into_iter() {
+            if is_match(last_char, c) {
+                new_polymer.pop();
+                no = false;
+                last_char = ' ';
+            } else {
+                new_polymer.push(c);
+                last_char = c;
+            }
+        }
+        polymer = new_polymer;
+    }
+    println!("{}", polymer.len());
+}
+
+fn is_match(a: char, b: char) -> bool {
+    if a.is_uppercase() && b.is_uppercase() {
+        return false;
+    }
+    if a.is_uppercase() {
+        return b.to_ascii_uppercase() == a;
+    }
+    if b.is_uppercase() {
+        return a.to_ascii_uppercase() == b;
+    }
+    false
 }
