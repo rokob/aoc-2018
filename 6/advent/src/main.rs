fcab06ba54bebad31c080cfe817bb4b91d1a04e0
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-05 21:53:40 -0800

    Setup for day 7

--- 6/advent/src/main.rs
+++ 6/advent/src/main.rs
@@ -1,6 +1,6 @@
 extern crate utils;
 #[allow(unused_imports)]
-use utils::{HashSet, HashMap, read_file, split_ws};
+use utils::{read_file, split_ws, HashMap, HashSet};
 
 const SIZE: usize = 400;
 
@@ -10,15 +10,15 @@ const UNSEEN: usize = 0;
 const DIST: usize = 10000;
 
 fn main() {
-    let mut grid = [[UNSEEN; SIZE]; SIZE];
+    let mut grid = [UNSEEN; SIZE * SIZE];
     let mut counter = TAG + 1;
-    let mut seen = Vec::new();
+    let mut seen = Vec::with_capacity(50);
     for line in read_file("advent.txt") {
         let parts = split_ws(&line);
         let x = parts[0].trim_end_matches(',').parse::<usize>().unwrap();
         let y = parts[1].parse::<usize>().unwrap();
-        grid[x][y] = counter;
-        seen.push((x,y));
+        grid[x * SIZE + y] = counter;
+        seen.push((x, y));
         counter += 1;
     }
 
@@ -26,11 +26,12 @@ fn main() {
         for y in 0..SIZE {
             let mut sum = 0;
             for (sx, sy) in seen.iter() {
-                let dist = (*sx as isize - x as isize).abs() as usize + (*sy as isize - y as isize).abs() as usize;
+                let dist = (*sx as isize - x as isize).abs() as usize
+                    + (*sy as isize - y as isize).abs() as usize;
                 sum += dist;
             }
             if sum <= DIST {
-                grid[x][y] = TAG;
+                grid[x * SIZE + y] = TAG;
             }
         }
     }
@@ -38,7 +39,7 @@ fn main() {
     let mut area = 0;
     for x in 0..SIZE {
         for y in 0..SIZE {
-            if grid[x][y] == TAG {
+            if grid[x * SIZE + y] == TAG {
                 area += 1;
             }
         }
