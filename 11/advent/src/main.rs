2a90121df6d947c4a3d5a6c34fe896e85386107f
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-11 12:34:02 -0800

    smarter day 11

--- 11/advent/src/main.rs
+++ 11/advent/src/main.rs
@@ -2,7 +2,7 @@ extern crate utils;
 #[allow(unused_imports)]
 use utils::{read_file, split_ws, HashMap, HashSet};
 /*
- * Find the fuel cell's rack ID, which is its X coordinate plus 10.
+Find the fuel cell's rack ID, which is its X coordinate plus 10.
 Begin with a power level of the rack ID times the Y coordinate.
 Increase the power level by the value of the grid serial number (your puzzle input).
 Set the power level to itself multiplied by the rack ID.
@@ -18,55 +18,66 @@ fn main() {
 
     for y in 1..=N {
         for x in 1..=N {
-            grid[y-1][x-1] = compute(x, y);
+            grid[y - 1][x - 1] = compute(x, y);
         }
     }
 
     let mut best = 0;
     let mut best_c = (0, 0, 0);
-    for size in 1..=300 {
-    for y in 1..=N {
-        for x in 1..=N {
-            if let Some(v) = score(&grid, x, y, size) {
-                if v > best {
-                    best = v;
-                    best_c = (x, y, size);
+
+    let mut sums = [[0; N]; N];
+
+    sums[0][0] = grid[0][0];
+
+    for x in 1..N {
+        sums[0][x] = grid[0][x] + sums[0][x - 1];
+    }
+    for y in 1..N {
+        sums[y][0] = grid[y][0] + sums[y - 1][0];
+    }
+    for y in 1..N {
+        for x in 1..N {
+            sums[y][x] = grid[y][x] + sums[y - 1][x] + sums[y][x - 1] - sums[y - 1][x - 1];
+        }
+    }
+
+    for k in 1..=N {
+        for r in k - 1..N {
+            for c in k - 1..N {
+                let total = score(&sums, c, r, k);
+                if total > best {
+                    best = total;
+                    // translate bottom right in zero based coords
+                    // to top left in 1 based coords
+                    best_c = (c + 2 - k, r + 2 - k, k);
                 }
             }
         }
     }
-    }
     println!("{},{},{}", best_c.0, best_c.1, best_c.2);
 }
 
-fn score(grid: &[[isize; N]; N], x: usize, y: usize, size: usize) -> Option<isize> {
-    if x + size - 2 < N && y + size - 2 < N {
-        let mut result = 0;
-        for r in (y-1)..=(y+size-2) {
-            for c in (x-1)..=(x+size-2) {
-                result += grid[r][c];
-            }
-        }
-        Some(result)
-    } else {
-        None
+fn score(sums: &[[isize; N]; N], x: usize, y: usize, k: usize) -> isize {
+    let mut total = sums[y][x];
+    if y >= k {
+        total -= sums[y - k][x];
     }
+    if x >= k {
+        total -= sums[y][x - k];
+    }
+    if x >= k && y >= k {
+        total += sums[y - k][x - k];
+    }
+    total
 }
 
 fn compute(x: usize, y: usize) -> isize {
-    let x = x as isize;
-    let y = y as isize;
-
-    let rack = x + 10;
-    let mut power = rack * y;
+    let rack = x as isize + 10;
+    let mut power = rack * y as isize;
     power += S;
     power *= rack;
-
-    for _ in 0..2 {
-        power /= 10;
-    }
-
-    power = power % 10;
-
+    power /= 10;
+    power /= 10;
+    power %= 10;
     power - 5
 }
