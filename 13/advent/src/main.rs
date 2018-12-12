719070316cb5124fc6f7a16d20bdf554aad4d459
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-12 09:21:07 -0800

    day 13 setup

---
+++ 13/advent/src/main.rs
@@ -0,0 +1,9 @@
+extern crate utils;
+#[allow(unused_imports)]
+use utils::{read_file, split_ws, HashMap, HashSet};
+
+fn main() {
+    for line in read_file("input.txt") {
+        let parts = split_ws(&line);
+    }
+}
