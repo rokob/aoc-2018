4ca4ee054134e5a1f428c5db42028d844d78ba33
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-13 21:12:23 -0800

    part 1

--- 14/advent/src/main.rs
+++ 14/advent/src/main.rs
@@ -2,10 +2,24 @@ extern crate utils;
 #[allow(unused_imports)]
 use utils::{read_file, split_ws, HashMap, HashSet};
 
+const N: usize = 919901;
+
 fn main() {
-    /*
-    for line in include_str!("../input.txt").lines() {
-        let parts = split_ws(&line);
+    let mut v = vec![3, 7, 1, 0];
+    let mut a = 0;
+    let mut b = 1;
+
+    while v.len() < N + 11 {
+        let sum = v[a] + v[b];
+        if sum > 9 {
+            v.push(1);
+            v.push(sum % 10);
+        } else {
+            v.push(sum);
+        }
+        a = (a + 1 + v[a]) % v.len();
+        b = (b + 1 + v[b]) % v.len();
     }
-    */
+
+    println!("{:?}", &v[N..N+10]);
 }
