7596c9cbc296b3b7f5bcd80ed22e1a2a629e665a
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-13 21:30:52 -0800

    part 2

--- 14/advent/src/main.rs
+++ 14/advent/src/main.rs
@@ -2,14 +2,23 @@ extern crate utils;
 #[allow(unused_imports)]
 use utils::{read_file, split_ws, HashMap, HashSet};
 
-const N: usize = 919901;
+const SEQ: &'static [usize] = &[9, 1, 9, 9, 0, 1];
+const N: usize = 6;
 
 fn main() {
-    let mut v = vec![3, 7, 1, 0];
+    let mut v = vec![3, 7, 1, 0, 1, 0, 1, 2];
     let mut a = 0;
-    let mut b = 1;
+    let mut b = 6;
 
-    while v.len() < N + 11 {
+    let offset = loop {
+        let l = v.len();
+        if &v[l - N..l] == SEQ {
+            break 0;
+        }
+        if &v[l - N - 1..l - 1] == SEQ {
+            break 1;
+        }
+
         let sum = v[a] + v[b];
         if sum > 9 {
             v.push(1);
@@ -19,7 +28,7 @@ fn main() {
         }
         a = (a + 1 + v[a]) % v.len();
         b = (b + 1 + v[b]) % v.len();
-    }
+    };
 
-    println!("{:?}", &v[N..N+10]);
+    println!("{}", v.len() - N - offset);
 }
