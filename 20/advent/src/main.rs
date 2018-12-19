d5a465cb622da0bc0649d41460170839da5e2291
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-19 11:36:13 -0800

    setup day 20

---
+++ 20/advent/src/main.rs
@@ -0,0 +1,13 @@
+extern crate utils;
+#[allow(unused_imports)]
+use utils::{read_file, split_ws, HashMap, HashSet};
+
+fn main() {
+    for (r, line) in include_str!("../input.txt").lines().enumerate() {
+        for (c, val) in line.chars().enumerate() {
+            match val {
+                _ => panic!("bad input"),
+            }
+        }
+    }
+}
