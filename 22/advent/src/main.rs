521e279c107690ed8009547b18e25b37f1a12f4a
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-20 22:46:07 -0800

    day 22 setup

---
+++ 22/advent/src/main.rs
@@ -0,0 +1,9 @@
+extern crate utils;
+#[allow(unused_imports)]
+use utils::{read_file, split_ws, HashMap, HashSet};
+
+fn main() {
+    let mut lines = include_str!("../input.txt").lines();
+    for line in lines {
+    }
+}
