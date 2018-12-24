111857b1ccba2f97c17453d5d605b60bbf1eff58
Andrew Weiss <wvvwwvw@gmail.com>
2018-12-23 18:38:35 -0800

    setup day 24

---
+++ 24/advent/src/main.rs
@@ -0,0 +1,11 @@
+extern crate utils;
+#[allow(unused_imports)]
+use utils::{read_file, split_ws, HashMap, HashSet};
+
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+struct X {}
+
+fn main() {
+    for line in include_str!("../input.txt").lines() {
+    }
+}
